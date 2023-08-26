637c2eef91c1910192fa7227baa7917df6a88359
David Mendoza <mendozaf.david@gmail.com>
2023-08-26 18:45:46 -0500

    actualiza version php

--- install.sh
+++ install.sh
@@ -128,7 +128,7 @@ services:
             - $HOME/proxy/fpms/$DIR:/etc/nginx/sites-available
         restart: always
     fpm$SERVICE_NUMBER:
-        image: rash07/php-fpm:1.0
+        image: rash07/php-fpm:8.2
         working_dir: /var/www/html
         volumes:
             - ./ssh:/root/.ssh
